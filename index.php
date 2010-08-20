3ba365cbcd560e9ed2368ec290a0191425ae5ec9
hopeseekr <hopeseekr@xmule.ws>
2010-08-20 16:12:08 -0500

    Added the licensing terms.

--- index.php
+++ index.php
@@ -1,11 +1,33 @@
 <?php
+/**
+* User Directory
+*   Copyright © 2008 Theodore R. Smith <theodore@phpexperts.pro>
+* 
+* The following code is licensed under a modified BSD License.
+* All of the terms and conditions of the BSD License apply with one
+* exception:
+*
+* 1. Every one who has not been a registered student of the "PHPExperts
+*    From Beginner To Pro" course (http://www.phpexperts.pro/) is forbidden
+*    from modifing this code or using in an another project, either as a
+*    deritvative work or stand-alone.
+*
+* BSD License: http://www.opensource.org/licenses/bsd-license.php
+**/
 
 // Initialize random data.
 $encodedNumbers = filter_input(INPUT_GET, 'numbers', FILTER_SANITIZE_STRING);
+$length = filter_input(INPUT_GET, 'length', FILTER_SANITIZE_NUMBER_INT);
+
+if ($length == '')
+{
+    $length = 10;
+}
+
 if ($encodedNumbers == '')
 {
     $randomNumbers = array();
-    for ($a = 0; $a <= 100; ++$a)
+    for ($a = 0; $a <= $length; ++$a)
     {
         $randomNumbers[] = rand(-50, 200);
     }
